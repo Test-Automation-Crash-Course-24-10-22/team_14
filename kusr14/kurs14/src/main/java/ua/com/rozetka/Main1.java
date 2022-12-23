f1fadba93657205d5c9207a2b586526901653048
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2022-12-23 20:14:36 +0200

    1.2.2.1

--- kusr14/kurs14/src/main/java/ua/com/rozetka/Main1.java
+++ kusr14/kurs14/src/main/java/ua/com/rozetka/Main1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\IdeaProjects\\kusr14\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\Documents\\GitHub\\team_14\\kusr14\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rozetka.com.ua/ua/");
