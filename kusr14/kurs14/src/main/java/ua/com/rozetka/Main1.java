2cf0a2f1691a6d86747adbea2b048d4c839264cb
TorVik247 <99217595+TorVik247@users.noreply.github.com>
2022-12-23 20:41:42 +0200

    1.2.4

--- kusr14/kurs14/src/main/java/ua/com/rozetka/Main1.java
+++ kusr14/kurs14/src/main/java/ua/com/rozetka/Main1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main1 {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\IdeaProjects\\kusr14\\chromedriver.exe");//C:\Users\home\IdeaProjects\kusr14\chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\Documents\\GitHub\\team_14\\kusr14\\chromedriver.exe");//C:\Users\home\IdeaProjects\kusr14\chromedriver.exe -- "C:\\Users\\home\\Documents\\GitHub\\team_14\\kusr14\\chromedriver.exe"
 
 
         WebDriver driver = new ChromeDriver();
